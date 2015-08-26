90de724d494be08faaf9e8b3cfbf151da90d3367
Alan Dipert <alan@dipert.org>
2015-08-25 23:55:40 -0400

    2.4.0-1

--- build.boot
+++ build.boot
@@ -1,16 +1,14 @@
 (set-env!
-  :dependencies '[[org.clojure/clojure       "1.6.0"     :scope "provided"]
-                  [boot/core                 "2.0.0-rc1" :scope "provided"]
-                  [adzerk/bootlaces          "0.1.8"     :scope "test"]
-                  [alandipert/jasmin         "2.4.0"]
-                  [alandipert/boot-trinkets  "1.0.0"]]
+ :dependencies '[[adzerk/bootlaces         "0.1.11" :scope "test"]
+                 [alandipert/jasmin        "2.4.0"]
+                 [alandipert/boot-trinkets "1.0.0"]]
   :source-paths #{"src"})
 
 (require '[adzerk.bootlaces :refer :all]
          '[alandipert.boot-jasmin :refer [jasmin]]
          '[alandipert.boot-trinkets :refer [run]])
 
-(def +version+ "2.4.0-0")
+(def +version+ "2.4.0-1")
 
 (bootlaces! +version+)
 
@@ -20,5 +18,4 @@
        :description "Boot task to assemble Jasmin files."
        :url         "https://github.com/alandipert/boot-jasmin"
        :scm         {:url "https://github.com/alandipert/boot-jasmin"}
-       :license     {:name "Eclipse Public License"
-                     :url  "http://www.eclipse.org/legal/epl-v10.html"}})
+       :license     {"Eclipse Public License" "http://www.eclipse.org/legal/epl-v10.html"}})
--- src/alandipert/boot_jasmin.clj
+++ src/alandipert/boot_jasmin.clj
@@ -1,26 +1,25 @@
 (ns alandipert.boot-jasmin
   {:boot/export-tasks true}
-  (:require [boot.pod                 :as pod]
-            [boot.core                :as core :refer [deftask]]
-            [boot.util                :as util]
-            [alandipert.boot-trinkets :refer [without-exiting]]))
+  (:require [boot.pod  :as pod]
+            [boot.core :as core :refer [deftask]]
+            [boot.util :as util]))
 
 (deftask jasmin
   "Assemble Jasmin source into class files."
   []
-  (let [tgt         (core/temp-dir!)
+  (let [tgt         (core/tmp-dir!)
         compile-pod (future (pod/make-pod (core/get-env)))]
     (fn [next-handler]
       (fn [fileset]
         (let [jasmin-files  (->> fileset
                                  core/input-files
                                  (core/by-ext [".j"]))
-              jasmin-argv   (->> (map #(.getPath (core/tmpfile %)) jasmin-files)
+              jasmin-argv   (->> (map #(.getPath (core/tmp-file %)) jasmin-files)
                                  (list* "-d" (.getPath tgt))
                                  vec)]
           (core/empty-dir! tgt)
           (util/info "Assembling Jasmin sources...\n")
-          (when (without-exiting
+          (when (util/without-exiting
                  (pod/with-eval-in @compile-pod
                    (jasmin.Main/main (into-array ~jasmin-argv)))
                  true)
